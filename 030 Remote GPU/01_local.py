20a3348560cc46108400f63bf483eb2163e14170
Anderson <a141890@gmail.com>
2017-09-11 15:44:20 +0900

    Add distributed computation with tensorflow

--- 030 Remote GPU/01_local.py
+++ 030 Remote GPU/01_local.py
@@ -5,8 +5,11 @@ with tf.device('/gpu:0'):
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c = tf.matmul(a, b)
 
-with tf.device('/cpu:0'):
+with tf.device('/gpu:0'):
     sum = tf.add(c, 3)
 # Creates a session with log_device_placement set to True.
+
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.1
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 print(sess.run(sum))
--- 030 Remote GPU/02_cluster.py
+++ 030 Remote GPU/02_cluster.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 
-cluster = tf.train.ClusterSpec({'local': ['localhost:2222', 'localhost:2223']})
+cluster = tf.train.ClusterSpec({'remote': ['ai:2222', 'ai:2223']})
 
-with tf.device('/job:local/task:0'):
+with tf.device('/job:remote/task:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 
-with tf.device('/job:local/task:1'):
+with tf.device('/job:remote/task:1'):
     c = tf.matmul(a, b) + 100
 
-with tf.Session('grpc://localhost:2222') as sess:
+with tf.Session('grpc://ai:2222') as sess:
     result = sess.run(c)
     print(result)
